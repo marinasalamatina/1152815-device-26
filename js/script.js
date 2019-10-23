65f309cb850557e3747fc7748c39de86c5021133
marinasalamatina <marinasalamatina@mail.ru>
2019-10-23 12:36:43 +0300

    Подключение слайдов и js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+var link = document.querySelector(".link_write_us");
+
+var popap = document.querySelector(".modal_write_us");
+var close = document.querySelector(".wtite_us_icon_close");
+
+var linkmap = document.querySelector(".link_map");
+
+var popapmap = document.querySelector(".modal_map_big");
+var closemap = document.querySelector(".map_big_icon_close");
+
+var dropdownlink = document.querySelector(".dropdown_plus_link");
+
+var dropdownpopap = document.querySelector(".dropdown_container");
